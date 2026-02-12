acb490434562bdd12ba3bc853ad9cac60aa6a864
Tejesh227 <tejeshreddy7386@gmail.com>
2026-02-12 17:48:17 +0530

    fix cors for production

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ connectDB();
 const app = express();
 
 /* ---------- IMPORTANT MIDDLEWARE ORDER ---------- */
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://taskflow-frontend.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
